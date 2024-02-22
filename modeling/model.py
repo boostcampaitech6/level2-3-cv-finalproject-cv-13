2acf7ffe5ef7e842ebca78b06c77777cfc5b7341
lukehanjun <70832671+lukehanjun@users.noreply.github.com>
2024-02-22 14:57:43 +0900

    feat: add model in config (#22)
    #21

--- modeling/model.py
+++ modeling/model.py
@@ -18,3 +18,33 @@ class MRNet(nn.Module):
         flattened_features = torch.max(pooled_features, 0, keepdim=True)[0]
         output = self.classifer(flattened_features)
         return output
+
+class Resnet50(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pretrained = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+        self.pooling_layer = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(1000, 2)
+    
+    def forward(self, x):
+        x = torch.squeeze(x, dim=0)
+        features = self.pretrained(x)
+        features = torch.unsqueeze(features, dim=2)
+        features = torch.unsqueeze(features, dim=3)
+        features = features.view(features.size(0), -1)
+        flattened_features = torch.max(features, 0, keepdim=True)[0]
+        out = self.classifier(flattened_features)
+        return out
+
+_model_entrypoints = {
+    "mrnet": MRNet,
+    "resnet50": Resnet50
+}
+
+def create_model(model, **kargs):
+    if model in _model_entrypoints:
+        model_constructor = _model_entrypoints[model]
+        model = model_constructor(**kargs)
+        return model
+    else:
+        raise RuntimeError(f"Unknown model ({model})")
