ac8b41bb5c668af3d1c005e9bd237df2510fb8a6
NewP1 <jaehyuk712@gmail.com>
2024-03-06 23:55:41 +0900

    feat: InferenceDataset 추가
    #51

--- modeling/dataloader.py
+++ modeling/dataloader.py
@@ -61,3 +61,52 @@ class MRDataset(data.Dataset):
 
         return array, label, self.weights
 
+class MRInferenceDataset(data.Dataset):
+    def __init__(self, root_dir, task, plane, transform=None, weights=None):
+        super().__init__()
+        self.task = task
+        self.plane = plane
+        self.root_dir = root_dir
+        self.folder_path = self.root_dir + 'test/{0}/'.format(plane)
+        self.records = pd.read_csv(
+            self.root_dir + 'test-{0}.csv'.format(task), header=None, names=['id', 'label'])
+
+        self.records['id'] = self.records['id'].map(
+            lambda i: '0' * (4 - len(str(i))) + str(i))
+        self.paths = [self.folder_path + filename +
+                      '.npy' for filename in self.records['id'].tolist()]
+        self.labels = self.records['label'].tolist()
+
+        self.transform = transform
+        if weights is None:
+            pos = np.sum(self.labels)
+            neg = len(self.labels) - pos
+            self.weights = torch.FloatTensor([1, neg / pos])
+        else:
+            self.weights = torch.FloatTensor(weights)
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        array = np.load(self.paths[index])
+        label = self.labels[index]
+        if label == 1:
+            label = torch.FloatTensor([[0, 1]])
+        elif label == 0:
+            label = torch.FloatTensor([[1, 0]])
+
+        if self.transform:
+            array = self.transform(array)
+        else:
+            array = np.stack((array,)*3, axis=1)
+            array = torch.FloatTensor(array)
+
+        # if label.item() == 1:
+        #     weight = np.array([self.weights[1]])
+        #     weight = torch.FloatTensor(weight)
+        # else:
+        #     weight = np.array([self.weights[0]])
+        #     weight = torch.FloatTensor(weight)
+
+        return array, label, self.weights
