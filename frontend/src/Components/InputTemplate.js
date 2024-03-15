101bdd27c1e03b4dc5547794f8c54bfa3d7446ea
lukehanjun <70832671+lukehanjun@users.noreply.github.com>
2024-03-15 11:24:32 +0900

    feat: sample data for frontend (#74)
    * feat: sample data for frontend
    
    * fix: fix frontend router

--- frontend/src/Components/InputTemplate.js
+++ frontend/src/Components/InputTemplate.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './InputTemplate.css'
 import ImgAsset from '../public'
+import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 import {useState, useRef} from 'react';
 import {Button} from "@mui/material";
@@ -11,9 +12,6 @@ export default function FirstImpression (props) {
 	const history = useHistory();
 	const [loading, setLoading] = useState(false);
 	let [ready, setReady] = useState([false, false, false]);
-	
-	const inputType = props.inputType;
-	const isDCM = (inputType == 'DICOM')
 
 	const saveImage = async (e, plane, idx) => {
 	e.preventDefault();
@@ -41,6 +39,9 @@ export default function FirstImpression (props) {
 			else if (response.status == '500') {
 				throw new Error('손상되었거나 지원되지 않는 DICOM 파일입니다.');
 			}
+			else if (response.status != '200') {
+				throw new Error('파일 업로드 과정에서 에러가 발생했습니다.');
+			}
 			ready[idx] = true;
 			setReady(ready);
         } catch (err) {
@@ -53,18 +54,24 @@ export default function FirstImpression (props) {
 
 	function changeScreen(e) {
 		e.preventDefault();
-		history.push("/loading")
+		history.push("/loading");
 	}
 
-	function changeInput(e) {
+	const sampleData = async (e, disease) => {
 		e.preventDefault();
-		if (isDCM) history.push("/PNG");
-		else history.push("/");
+		const sampleurl = `http://127.0.0.1:8000/input/sample?disease=${disease}`
+		try {
+			const response = await axios.get(sampleurl);
+			setReady([true, true, true]);
+			alert("샘플 파일 업로드에 성공했습니다. \nInference 버튼을 눌러 주세요");
+		} catch (err) {
+			alert(`샘플 파일 업로드에 실패했습니다. \n ${err}`);
+		}
 	}
 
 	function alertUpload(e) {
 		e.preventDefault();
-		alert(`${inputType} 파일을 먼저 업로드해 주세요`)
+		alert("DICOM 파일을 먼저 업로드해 주세요")
 	}
 
 	return (
@@ -139,9 +146,14 @@ export default function FirstImpression (props) {
 					Inference
 				</Button>}
 			</div>
-			<div className='ToPNG'>
-				<Button variant="text"  onClick={changeInput}  sx={{ color: 'white', fontFamily: "Alata, Helvetica", fontSize: '15px'}}>
-					Upload {isDCM ? 'Image' : 'DICOM'} file instead
+			<div className='sample-acl'>
+				<Button variant="text"  onClick={(e) => sampleData(e, 'acl')}  sx={{ color: 'white', fontFamily: "Alata, Helvetica", fontSize: '15px'}}>
+					Upload Sample ACL file instead
+				</Button>
+			</div>
+			<div className='sample-meniscus'>
+				<Button variant="text"  onClick={(e) => sampleData(e, 'meniscus')}  sx={{ color: 'white', fontFamily: "Alata, Helvetica", fontSize: '15px'}}>
+					Upload Sample Meniscus file instead
 				</Button>
 			</div>
         </div>
