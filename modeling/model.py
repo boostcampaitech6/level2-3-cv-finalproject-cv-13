03bc785bf7c4b3c6f528ecc4ef39d9db019fe807
lukehanjun <lukehanjun@gmail.com>
2024-03-19 10:30:24 +0900

    feat: add hrnet18

--- modeling/model.py
+++ modeling/model.py
@@ -292,6 +292,24 @@ class VGG11(nn.Module):
         output = self.classifer(flattened_features)
         return output
     
+class HRNet18(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pretrained_model = timm.create_model('hrnet_w18_small_v2.ms_in1k', pretrained=True)
+        self.classifer = nn.Sequential(
+            nn.Linear(1000, 256),
+            nn.Linear(256, 2)
+        )
+
+        self.target = [self.pretrained_model.final_layer[-1]]
+
+    def forward(self, x):
+        x = torch.squeeze(x, dim=0) 
+        features = self.pretrained_model(x)
+        flattened_features = torch.max(features, 0, keepdim=True)[0]
+        output = self.classifer(flattened_features)
+        return output
+    
 _model_entrypoints = {
     "mrnet": MRNet,
     "resnet50": Resnet50,
@@ -304,7 +322,8 @@ _model_entrypoints = {
     "efficientnet_b0": EfficientNet_b0,
     "efficientnet_v2_s" : Efficientnet_v2_s,
     "vgg19" : VGG19,
-    "vgg11" : VGG11
+    "vgg11" : VGG11,
+    "hrnet18": HRNet18,
 }
 
 def create_model(model, **kargs):
