590f321e38fb1c443e30152f7305ededb80541e1
_dlwogur_ <jaehyuk712@gmail.com>
2024-03-21 10:53:07 +0900

    update: update seed code
    #96

--- modeling/train.py
+++ modeling/train.py
@@ -26,6 +26,7 @@ deterministic = True
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 if deterministic:
 	torch.backends.cudnn.deterministic = True
