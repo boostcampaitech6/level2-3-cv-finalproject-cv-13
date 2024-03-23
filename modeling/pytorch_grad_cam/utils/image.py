9791dad391073087fc2c1a88757109d404050309
yuniroro <ilho7159@gmail.com>
2024-03-24 05:23:27 +0900

    feat: add 3d gradcam
    #87

--- modeling/pytorch_grad_cam/utils/image.py
+++ modeling/pytorch_grad_cam/utils/image.py
@@ -176,6 +176,8 @@ def show_factorization_on_image(img: np.ndarray,
 
 
 def scale_cam_image(cam, target_size=None):
+    cam = np.squeeze(cam)
+
     result = []
     for img in cam:
         img = img - np.min(img)
@@ -184,7 +186,6 @@ def scale_cam_image(cam, target_size=None):
             img = cv2.resize(img, target_size)
         result.append(img)
     result = np.float32(result)
-
     return result
 
 
